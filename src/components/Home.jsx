49b0b8d190018c08d420f5b9a64ba5580aa987d9
pamela2002 <sroychoudhury65@gmail.com>
2023-09-07 18:46:33 +0530

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,20 @@
+
+
+
+
+
+import Code from './Code';
+import Result from './Result';
+import Header from './Header';
+
+const Home = () => {
+    return (
+        <>
+            <Header />
+            <Code />
+            <Result />
+        </>
+    )
+}
+
+export default Home;
